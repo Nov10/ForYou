377436db4d014babed03e73ed6c0d1732de822dc
nov10 <mon10lur49@gmail.com>
2025-11-02 22:45:09 +0900

    Add Zoom Effect in MainPlayScene Camera
    MainPlayScene에서 시작할 때, Zoom Effect를 추가

--- Assets/Scripts/InGame/CameraController.cs
+++ Assets/Scripts/InGame/CameraController.cs
@@ -1,4 +1,5 @@
 using Helpers;
+using System.Collections;
 using UnityEngine;
 namespace ForYou.GamePlay
 {
@@ -11,6 +12,50 @@ namespace ForYou.GamePlay
         [SerializeField] float CameraZPosition = -10.0f;
 
         public bool Follow = true;
+
+        [SerializeField] bool StartZoomEffect = false;
+        [SerializeField] float CameraSize = 7.0f;
+        private void Awake()
+        {
+            if (StartZoomEffect == true)
+            {
+                Follow = false;
+                DelayedFunctionHelper.InvokeDelayed(3.0f, () =>
+                {
+                    StartCoroutine(_ChangeCameraSize(CameraSize, 2.0f));
+                    Follow = true;
+                    FollowSnapping = 2.0f;
+                    DelayedFunctionHelper.InvokeDelayed(2.0f, () =>
+                    {
+                        FollowSnapping = 5.0f;
+                    });
+                });
+            }
+            else
+            {
+                GetComponentInChildren<Camera>().orthographicSize = CameraSize;
+            }
+        }
+
+        IEnumerator _ChangeCameraSize(float targetSize, float duration)
+        {
+            var camera = GetComponentInChildren<Camera>();
+            var nowSize = camera.orthographicSize;
+            float elapsed = 0.0f;
+            while (elapsed < duration)
+            {
+                elapsed += Time.deltaTime;
+                //var newSize = Mathf.Lerp(nowSize, targetSize, elapsed / duration);
+
+                //그냥 lerp는 좀 단순하니까...
+                var t = elapsed / duration;
+                t = t * t * (3f - 2f * t); //smoothstep
+                var newSize = Mathf.Lerp(nowSize, targetSize, t);
+                camera.orthographicSize = newSize;
+                yield return new WaitForFixedUpdate();
+            }
+            camera.orthographicSize = targetSize;
+        }
 
         private void FixedUpdate()
         {
